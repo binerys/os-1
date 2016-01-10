e7f9019f92a348de5c6f48db4447f177ed40fc0b
Garima Lunawat <glunawat@ucla.edu>
2016-01-09 22:57:05 -0800

    handle file descriptors

--- handling.c
+++ handling.c
@@ -2,4 +2,11 @@
 
 #include "handling.h"
 
+int fileDescriptors[100];
+int index = 0;
 
+void handle_fd(int fd)
+{
+    fileDescriptors[index] = fd;
+    index++;
+}
--- handling.h
+++ handling.h
@@ -1,6 +1,6 @@
 #ifndef HANDLING_H
 #define HANDLING_H
 
-
+void handle_fd(int fd);
 
 #endif
