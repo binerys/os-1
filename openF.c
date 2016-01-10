9f511dc369e4d248ba92e25035623793cdf02555
Garima Lunawat <glunawat@ucla.edu>
2016-01-09 22:21:13 -0800

    omplemented opening files

--- openF.c
+++ openF.c
@@ -4,10 +4,31 @@
 
 int open_rdonly_f (char* file)
 {
-    
+    //not sure 
+    int fd = open(file, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Error");
+        return -1;
+    }    
+    else
+    {
+        return fd;
+    }
 }
 
 int open_wronly_f (char* file)
 {
+    //not sure 
+    int fd = open(file, O_WRONLY);
+    if (fd == -1)
+    {
+        printf("Error");
+        return -1;
+    }    
+    else
+    {
+        return fd;
+    }
 
 }
