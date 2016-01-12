6466e1c357690cb656628c00b9cfc60823fd5425
Breanna Nery <bree.nery@gmail.com>
2016-01-11 18:26:06 -0800

    Check command arguments and parse into variables

--- parser.c
+++ parser.c
@@ -66,9 +66,13 @@ int parser(int argc, char** argv)
                 char* commandArgs[4];
                 int argLen = 0;
                 int argCount = 0;
+                int input = -1; 
+                int output = -1;
+                int error = -1; 
+                char* cmd; 
+
                 for (int i= 0; optind < argc; optind++)
                 {
-                    // 
                     if(argv[optind][0] == '-' && argv[optind][1] == '-'){
                         printf("Discovered new option: %s \n",argv[optind]);
                         break;
@@ -77,24 +81,48 @@ int parser(int argc, char** argv)
                         printf("Error: Too many arguments");
                         break;
                     }
-                    printf("Current arg: %s \n",argv[optind]);
+                    
+                    // Copy argument from argv to command args
                     argLen = strlen(argv[optind]);
                     commandArgs[i] = malloc(argLen*sizeof(char));
                     strcpy(commandArgs[i], argv[optind]);
-                    printf("The copied arg: %s \n", commandArgs[i]);
                     argCount++;
                     i++;
                 }
                 if (argCount != 4)
                     printf("Error: Too little arguments");
 
+                // Extract commands from commandArgs and call command
+                int temp; 
+                char* end;
+                for(int i = 0; i < argCount; i++)
+                {
+                    // Arg #1,2,3: Check that arg is a single number
+                    if(i == 0 | i == 1 | i == 2)
+                    {
+                        argLen = strlen(commandArgs[i]);
+                        if( argLen != 1)
+                            printf("Error! Argument is not a single character!");
+                        
+                        temp = strtol(commandArgs[i], &end,0);
+                        if(end == commandArgs[i]) // Not a digit
+                            printf("Error! Argument is not a digit!");
+
+                        if(i == 0)  input = temp;
+                        if(i == 1)  output = temp;
+                        if(i == 2)  error = temp; 
+
+                    }
+
+
+                }
                 break;
             case 'v':
                 //set bool to true
                 //printf("verbose");
                 if (verboseTrue == 1)
                 {
-                    printf("--verbose\n");
+                    printf("--verbose \n");
                 }
                 verboseTrue = 1;
                 break;
