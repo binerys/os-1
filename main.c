66c478d8a631ed05ebc44cb3f7c299fd443e0f82
Breanna Nery <bree.nery@gmail.com>
2016-01-12 23:10:39 -0800

    xtra stuff

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int exit_status;
 int main(int argc, char** argv)
 {
     exit_status = parser(argc, argv);
-    printf("Exit Status: %d", exit_status);
+    printf("Exit Status: %d \n", exit_status);
     printf("%d", exit_status);
     return exit_status;    
 }
