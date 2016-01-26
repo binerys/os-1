68656737503cf3a00abb18a1c98cc93b4a6bd2fe
Breanna Nery <bnery@wifi-131-179-20-149.host.ucla.edu>
2016-01-25 17:33:08 -0800

    Deleting old openF files

--- openF.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENF_H
-#define OPENF_H
-
-int setFlags (int flags[]);
-int open_rdonly_f(char* file, int flags[]);
-int open_wronly_f(char* file, int flags[]);
-int open_rdwr_f(char* file, int flags[]);
-
-#endif 
